7c3a1b1b06053f3e0dd6ed7178937a477d231469
Golnush Asefi <golasefi@gmail.com>
2022-05-02 18:33:05 +0430

    new commit

--- src/pages/ProductPage/products.jsx
+++ src/pages/ProductPage/products.jsx
@@ -2,6 +2,8 @@ import classes from "./products.module.scss";
 import Item from "./item";
 import { useEffect, useState } from "react";
 import axiosInstance from "../../axios";
+import MainNavigation from "../../components/layout/MainNavigation";
+import Footer from "../../components/layout/Footer";
 
 function Products() {
   const [userStyles, setUserStyle] = useState([]);
@@ -19,20 +21,23 @@ function Products() {
     });
   }, []);
   return (
-    <div className={classes.Products}>
-      <div className={classes.Products__itemContainer}>
-        {userStyles.map((element) => (
-          <Item
-            name={" شلوار مردانه سیدونا مدل MSI03072-403"}
-            price={99000}
-            img={element.style_image_url}
-          ></Item>
-        ))}
+    <>
+      <MainNavigation />
+      <div className={classes.Products}>
+        <div className={classes.Products__itemContainer}>
+          {userStyles.map((element) => (
+            <Item
+              name={" شلوار مردانه سیدونا مدل MSI03072-403"}
+              price={99000}
+              img={element.style_image_url}
+            ></Item>
+          ))}
+        </div>
+        <div className={classes.Products__filterContainer}>
+          <h2>فیلترها</h2>
+        </div>
       </div>
-      <div className={classes.Products__filterContainer}>
-        <h2>فیلترها</h2>
-      </div>
-    </div>
+    </>
   );
 }
 export default Products;
