205a40da2379e266b2e2bacf8596ebe2f9b88a46
Mahdi-Ghanbarzadeh <mahdi.ghanbarzadeh.a@gmail.com>
2022-05-04 18:23:01 +0430

    added confirm box, responsive design

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import Homepage from "./pages/Homepage";
 import Survey from "./pages/Survey";
 import AccountBox from "./pages/AccountBox";
 import Products from "./pages/ProductPage/products";
+import ShoppingList from "./pages/ShoppingList";
+import Product from "./pages/ProductPageEdit";
 
 export const routes = [
   { path: "/", element: <Homepage /> },
@@ -11,7 +13,9 @@ export const routes = [
     element: <AccountBox />,
   },
   {
-    path: "/product",
+    path: "/products",
     element: <Products />,
   },
+  { path: "/ShoppingList", element: <ShoppingList /> },
+  { path: "/product", element: <Product /> },
 ];
