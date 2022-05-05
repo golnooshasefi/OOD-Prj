a31b3c73c947c8431bf66cc68430b3b4a65da287
Mahdi-Ghanbarzadeh <mahdi.ghanbarzadeh.a@gmail.com>
2022-05-05 23:32:08 +0430

    product page added

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Survey from "./pages/Survey";
 import AccountBox from "./pages/AccountBox";
 import Products from "./pages/ProductPage/products";
 import ShoppingList from "./pages/ShoppingList";
+import Product from "./pages/ProductPageEdit";
 
 export const routes = [
   { path: "/", element: <Homepage /> },
@@ -12,8 +13,9 @@ export const routes = [
     element: <AccountBox />,
   },
   {
-    path: "/product",
+    path: "/products",
     element: <Products />,
   },
   { path: "/ShoppingList", element: <ShoppingList /> },
+  { path: "/product", element: <Product /> },
 ];
